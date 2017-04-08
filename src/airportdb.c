skiselkov/X-RAAS2

commit 9719a1b63a4545c064cfadd2a924f6bc320e1ba2

Added fallback loading from default data on XP11 in case custom navdata is present, but empty.

src/airportdb.c
@@ -1195,6 +1195,10 @@ parse_airports_txt_A_line(airportdb_t *db, const char *filename,
 	if (arpt == NULL)
 		goto out;
 
+	/* airport already seen in previous version of the database, skip */
+	if (arpt->in_navdb)
+		goto out;
+
 	if (arpt->geo_linked)
 		geo_unlink_airport(db, arpt);
 	arpt->refpt = GEO_POS3(atof(comps[3]), atof(comps[4]),
@@ -1337,6 +1341,9 @@ load_arinc42418_arpt_data(const char *filename, airport_t *arpt)
 		return (B_FALSE);
 	}
 
+	/* airport already seen in previous version of the database, skip */
+	if (arpt->in_navdb)
+		return (B_TRUE);
 	arpt->in_navdb = B_TRUE;
 
 	while (!feof(fp)) {
@@ -1532,27 +1539,26 @@ static bool_t
 load_xp11_navdata(airportdb_t *db)
 {
 	bool_t isdir;
-	char *dirpath_custom;
+	char *dirpath;
 	bool_t success = B_FALSE;
 
-	dirpath_custom = mkpathname(db->xpdir,
-	    "Resources", "Custom Data", "CIFP", NULL);
-	if (file_exists(dirpath_custom, &isdir) && isdir) {
-		success = load_CIFP_dir(db, dirpath_custom);
-		if (!success)
+	dirpath = mkpathname(db->xpdir, "Resources", "Custom Data", "CIFP",
+	    NULL);
+	if (file_exists(dirpath, &isdir) && isdir) {
+		if (!load_CIFP_dir(db, dirpath))
 			logMsg("%s: error parsing navdata, falling "
-			    "back to default data.", dirpath_custom);
+			    "back to default data.", dirpath);
 	}
-	free(dirpath_custom);
+	free(dirpath);
+
+	dirpath = mkpathname(db->xpdir, "Resources", "default data", "CIFP",
+	    NULL);
+	success = load_CIFP_dir(db, dirpath);
 	if (!success) {
-		char *dirpath_default = mkpathname(db->xpdir,
-		    "Resources", "default data", "CIFP", NULL);
-		success = load_CIFP_dir(db, dirpath_default);
-		if (!success)
-			logMsg("%s: error parsing navdata, "
-			    "please check your install", dirpath_default);
-		free(dirpath_default);
+		logMsg("%s: error parsing navdata, please check your install",
+		    dirpath);
 	}
+	free(dirpath);
 
 	return (success);
 }
@@ -1867,12 +1873,21 @@ recreate_cache(airportdb_t *db)
 		 * airport, because we don't have TA/TL info on them.
 		 */
 		if (!arpt->in_navdb) {
-			dbg_log(tile, 5, "Airport %s is missing geo_xrefs, "
-			    "dropping it", arpt->icao);
+			logMsg("Error: airport %s not found in navdata, "
+			    "dropping it.", arpt->icao);
 			geo_unlink_airport(db, arpt);
 			avl_remove(&db->apt_dat, arpt);
 			free_airport(arpt);
 		}
+	}
+	if (avl_numnodes(&db->apt_dat) == 0) {
+		log_init_msg(B_TRUE, INIT_ERR_MSG_TIMEOUT, NULL, NULL,
+		    "X-RAAS navigational database error: it appears your "
+		    "simulator's navigational database is broken,\n"
+		    "or your simulator contains no airport scenery. Please "
+		    "reinstall the database and retry.");
+		success = B_FALSE;
+		goto out;
 	}
 
 	if (!recreate_cache_skeleton(db, &apt_dat_files)) {

src/log.c
@@ -40,24 +40,25 @@
 #define	PREFIX_FMT	"%s %s[%s:%d]: ", timedate, PREFIX, filename, line
 
 debug_config_t xraas_debug_config;
-static FILE *my_log_fp = NULL;
+static FILE *private_log_fp = NULL;
+bool_t xraas_private_log = B_FALSE;
 
 static void
-close_my_log(void)
+close_private_log(void)
 {
-	VERIFY(my_log_fp != NULL);
-	fclose(my_log_fp);
-	my_log_fp = NULL;
+	VERIFY(private_log_fp != NULL);
+	fclose(private_log_fp);
+	private_log_fp = NULL;
 }
 
 static void
-open_my_log(void)
+open_private_log(void)
 {
 	char *filename = mkpathname(xraas_xpdir, "Output", "caches",
 	    "X-RAAS_log.txt", NULL);
-	my_log_fp = fopen(filename, "wb");
-	if (my_log_fp)
-		atexit(close_my_log);
+	private_log_fp = fopen(filename, "wb");
+	if (private_log_fp)
+		atexit(close_private_log);
 	free(filename);
 }
 
@@ -82,9 +83,6 @@ log_impl_v(const int *class, int level, const char *filename, int line,
 	struct tm *tm;
 	time_t t;
 
-	if (my_log_fp == NULL)
-		open_my_log();
-
 	t = time(NULL);
 	tm = localtime(&t);
 	VERIFY(strftime(timedate, sizeof (timedate), DATE_FMT, tm) != 0);
@@ -99,8 +97,12 @@ log_impl_v(const int *class, int level, const char *filename, int line,
 	(void) vsnprintf(&buf[prefix_len], len + 1, fmt, ap);
 	(void) sprintf(&buf[strlen(buf)], "\n");
 
-	if (my_log_fp != NULL)
-		fputs(buf, my_log_fp);
+	if (xraas_private_log) {
+		if (private_log_fp == NULL)
+			open_private_log();
+		if (private_log_fp != NULL)
+			fputs(buf, private_log_fp);
+	}
 	if (class == NULL || *class >= level ||
 	    xraas_debug_config.all >= level) {
 		XPLMDebugString(buf);
@@ -198,9 +200,13 @@ log_backtrace(void)
 		}
 	}
 
-	if (my_log_fp != NULL) {
-		fputs(backtrace_buf, my_log_fp);
-		fflush(my_log_fp);
+	if (xraas_private_log) {
+		if (private_log_fp == NULL)
+			open_private_log();
+		if (private_log_fp != NULL) {
+			fputs(backtrace_buf, private_log_fp);
+			fflush(private_log_fp);
+		}
 	}
 	XPLMDebugString(backtrace_buf);
 	fputs(backtrace_buf, stderr);
@@ -224,9 +230,13 @@ log_backtrace(void)
 	for (i = 1, j = BACKTRACE_STRLEN; i < sz; i++)
 		j += sprintf(&msg[j], "%s\n", fnames[i]);
 
-	if (my_log_fp != NULL) {
-		fputs(msg, my_log_fp);
-		fflush(my_log_fp);
+	if (xraas_private_log) {
+		if (private_log_fp == NULL)
+			open_private_log();
+		if (private_log_fp != NULL) {
+			fputs(msg, private_log_fp);
+			fflush(private_log_fp);
+		}
 	}
 	XPLMDebugString(msg);
 	fputs(msg, stderr);

src/log.h
@@ -46,6 +46,7 @@ typedef struct {
 } debug_config_t;
 
 extern debug_config_t xraas_debug_config;
+extern bool_t xraas_private_log;
 
 /*
  * This lets us chop out the basename (last path component) from __FILE__
@@ -69,9 +70,13 @@ void log_backtrace(void);
 
 #define	dbg_log(class, level, ...) \
 	do { \
-		log_impl(&xraas_debug_config.class, level, \
-		    log_basename(__FILE__), __LINE__, \
-		    "[" #class "/" #level "] " __VA_ARGS__); \
+		if (xraas_debug_config.class >= level || \
+		    xraas_debug_config.all >= level || \
+		    xraas_private_log) { \
+			log_impl(&xraas_debug_config.class, level, \
+			    log_basename(__FILE__), __LINE__, \
+			    "[" #class "/" #level "] " __VA_ARGS__); \
+		} \
 	} while (0)
 
 #ifdef __cplusplus

commit e8c1366e4eaf3385eda7ba738e73f15460bffb96

Fixed regression introduced in 9719a1b6 where file descriptors would leak.

src/airportdb.c
@@ -1333,17 +1333,19 @@ load_arinc42418_arpt_data(const char *filename, airport_t *arpt)
 {
 	char *line = NULL;
 	size_t linecap = 0;
-	FILE *fp = fopen(filename, "r");
+	FILE *fp;
 	int line_num = 0;
 
+	/* airport already seen in previous version of the database, skip */
+	if (arpt->in_navdb)
+		return (B_TRUE);
+
+	fp = fopen(filename, "r");
 	if (fp == NULL) {
 		logMsg("Can't open %s: %s", filename, strerror(errno));
 		return (B_FALSE);
 	}
 
-	/* airport already seen in previous version of the database, skip */
-	if (arpt->in_navdb)
-		return (B_TRUE);
 	arpt->in_navdb = B_TRUE;
 
 	while (!feof(fp)) {